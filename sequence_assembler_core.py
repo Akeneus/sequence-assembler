0e61a5fbcacae6a94f93578d859fb92fc77619cc
Niclas <bilbobeutlin15@gmx.de>
2021-06-05 14:30:15 +0200

    reafactor file

--- sequence_assembler_core.py
+++ sequence_assembler_core.py
@@ -14,8 +14,9 @@ import math
 #4 bereinigen der Kanten (check)
 #5 wieder bei 3 solange es kanten gibt (check)
 #6 fertig (check)
-# 7 Qualitätskontrolle (check)
-minWeight = 2
+# 7 Qualitätskontrolle ?
+# eulerpfad ?
+minWeight = 3
 numberOfIterations = 1
 # ToDo ausführeung durch mitgabe einer frag_b datei in console
 def main(minWeight, numberOfIterations):
@@ -39,10 +40,9 @@ def _buildGraph(path) -> Graph:
     frag_file = open(path, 'r')
     lines = frag_file.readlines()
     lines = [Lines.strip() for Lines in lines] 
-    newLines = _doubleHelixStuff(lines)
     g = Graph(directed= True)
-    g = _buildVertices(g,newLines)
-    g = _buildEdges(g,newLines)
+    g = _buildVertices(g,lines)
+    g = _buildEdges(g,lines)
     return g
 
 def _buildVertices(g:Graph,lines:List[str]) -> Graph:
@@ -64,8 +64,6 @@ def _buildEdges(g:Graph,lines:List[str]) -> Graph:
                 edge = (tmpLines.index(lineToCheck),tmpLines.index(line))
                 edgeList.append(edge)
                 weightList.append(matchingAffix)
-
-            
     g.add_edges(edgeList)
     g.es["weight"] = weightList
     return g
@@ -86,7 +84,6 @@ def _checkSequence(stringA:str, stringB:str) -> int:
         if(stringB.startswith(tmp)):
             return len(tmp)
     return v
-
 
 def _assemble(data:AssembleData):
     # wenn noch kanten da, dann weiter machen!
@@ -164,37 +161,5 @@ def _buildPath(path:str):
     dirName = dirName+"\\run_"+str(numFolders)+"_"+date.today().strftime("%d-%m-%Y")+"\\"
     os.makedirs(dirName)
     return dirName
-
-
-def _doubleHelixStuff(lines:list):
-    newLines = []
-    newLines.append(lines[0])
-    tmpLines = lines.copy()
-    for sequence in tmpLines[1::]:
-        same = 0
-        opp = 0
-        for savedSequence in newLines:
-            same += _checkSequenceLR(savedSequence, sequence)
-            opp += _checkSequenceLR(_getComp(sequence), savedSequence)
-
-        if(same > opp):
-            newLines.append(sequence)
-        else:
-            newLines.append(_getComp(sequence))
-    return newLines
- 
-
-def _checkSequenceLR(stringA:str, stringB:str) -> int:
-    a = _checkSequence(stringA,stringB)
-    b = _checkSequence(stringB,stringB)
-    return a+b
-
-def _getComp(stringB:str):
-    tmpstring = stringB
-    tmpstring = tmpstring.replace("A","t")
-    tmpstring = tmpstring.replace("T","a")
-    tmpstring = tmpstring.replace("G","c")
-    tmpstring = tmpstring.replace("C","g")
-    return (tmpstring.upper())[::-1]
 
 main(1,1)
